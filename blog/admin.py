0258ab06d7dc3c2dd9289b77f2e42d5780b08828
isaaclee12 <isaac.lee@uvm.edu>
2021-04-17 11:32:12 -0400

    Successfully update Django to latest version v3.1.0 and made website work still

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin  import SummernoteModelAdmin
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
