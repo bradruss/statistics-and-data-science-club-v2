a0e7f9e5ad3feadce1019caa2468499d5d6acf86
isaaclee12 <isaac.lee@uvm.edu>
2021-05-25 19:07:50 -0400

    Played around with UVM Silk server settings. Attempting to add blog posts to front page.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -av --exclude '.git*' --exclude 'deploy.sh' --exclude 'venv' --exclude 'db.sqlite3' --exclude '.htaccess' \
+rsync -av --rsh=ssh --exclude '.git*' --exclude 'deploy.sh' --exclude 'venv' --exclude 'db.sqlite3' --exclude '.htaccess' \
 	  . statclub:django-root/
-rsync .htaccess statclub:www-root/
-rsync -av ../www-root/static statclub:www-root/
+rsync .htaccess --rsh=ssh statclub:www-root/
+rsync -av --rsh=ssh  ../www-root/static statclub:www-root/
