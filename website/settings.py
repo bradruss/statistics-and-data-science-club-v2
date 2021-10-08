6820e6c35f624c27fa05d590eb72e65549a44908
isaaclee12 <isaac.lee@uvm.edu>
2021-10-08 13:26:22 -0400

    Trying to fix the blog but nothing is working I am very sad

--- website/settings.py
+++ website/settings.py
@@ -129,6 +129,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../www-root/static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../www-root/media/')
 
+SUMMERNOTE_CONFIG = {
+    'iframe': False,
+    "jquery": "summernoteJQuery",
+
+    'summernote': {
+
+        'width': '100%'
+    }
+}
+
+SUMMERNOTE_THEME = 'bs3'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -187,3 +199,6 @@ EMAIL_HOST_PASSWORD = SECRET_APIKEY
 # Email options
 #EMAIL_USE_SSL = True
 EMAIL_USE_TLS = True
+
+# This might fix the blog
+X_FRAME_OPTIONS = "SAMEORIGIN"
